f2156b426cedfe7b2331f0bdb9ecdf2ddd9c27c6
Shisui-1111 <castorpop30@gmail.com>
2025-09-13 14:47:37 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Iv0S0LIb#HTaPY1-SSXntfOyGhGPjAHeO4bJSvIYLl3fKoNID-8Q",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "243985034532 ", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
